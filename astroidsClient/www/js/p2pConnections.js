674b85a81e1ae3c6716fb0acdb43ddb47dd46d55
Mirco Kraenz <prosingularity@yahoo.de>
2014-09-28 20:16:39 +0200

    ADDED + CLEANED UP: p2p handler for phaser calls

--- astroidsClient/www/js/p2pConnections.js
+++ astroidsClient/www/js/p2pConnections.js
@@ -1,38 +1,49 @@
+astroids.p2p = {};
+
 // You can pick your own id or omit the id if you want to get a random one from
 // the server.
-var peer = new Peer(astroids.playerId, {
-    key : '8vlzpxgdnobhuxr'
+astroids.p2p.peer = new Peer(astroids.playerId, {
+	key : '8vlzpxgdnobhuxr'
 });
 
 // receive data via webRTC
-peer.on('connection', function(conn) {
-    conn.on('data', function(data) {
-        console.log('received data: ' + data);
-        
-        if (data.match('helloIam*')) {
-            astroids.connectBackTo(data);
-        }
-    });
+astroids.p2p.peer.on('connection', function(conn) {
+	conn.on('data', function(data) {
+		console.log('received data: ' + data);
+
+		if (data.match('helloIam*')) {
+			astroids.p2p.connectBackTo(data);
+		}
+	});
 });
 
-//connect to other player if available
-var conn = peer.connect(astroids.otherPlayerId);
-conn.on('open', function() {
-    console.log('sending data: ' + 'helloIam' + astroids.playerId);
-    conn.send('helloIam' + astroids.playerId);
+// try to connect to other player under any circumstances
+astroids.p2p.connection = astroids.p2p.peer.connect(astroids.otherPlayerId);
+astroids.p2p.connection.on('open', function() {
+	console.log('sending data: ' + 'helloIam' + astroids.playerId);
+	astroids.p2p.connection.send('helloIam' + astroids.playerId);
 });
 
-astroids.connectBackTo = function(data) {
-    var otherPlayerId = data.slice('helloIam'.length);
-    conn = peer.connect(otherPlayerId);
-    conn.on('open', function() {
-        console.log('sending data: ' + 'helloThere, I am ' + astroids.playerId);
-        conn.send('helloThere, I am ' + astroids.playerId);
-    });
+astroids.p2p.connectBackTo = function(data) {
+	var otherPlayerId = data.slice('helloIam'.length);
+	astroids.p2p.connection = astroids.p2p.peer.connect(otherPlayerId);
+	astroids.p2p.connection.on('open',
+			function() {
+				console.log('sending data: ' + 'helloThere, I am '
+						+ astroids.playerId);
+				astroids.p2p.connection.send('helloThere, I am '
+						+ astroids.playerId);
+			});
 }
 
-// send data via webRTC
-astroids.onPingOtherPlayer = function() {
-    console.log('i was clicked!');
-    conn.send('hi ' + astroids.playerId + ' was clicked!');
+// send ping via webRTC
+astroids.p2p.onPingOtherPlayer = function() {
+	console.log('i was clicked!');
+	astroids.p2p.connection.send('ping from ' + astroids.playerId);
+}
+
+// send string via webRTC
+astroids.p2p.sendText = function(text) {
+	console.log('sending text: ' + text);
+	astroids.p2p.connection.send(text);
 }
