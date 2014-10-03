0475ec70d9b36b84eea45f79c626e6ffb64702c8
Mirco Kraenz <prosingularity@yahoo.de>
2014-10-03 12:48:26 +0200

    ADDED: player rotation and controls and synchronization

--- astroidsClient/www/ts/Level1.ts
+++ astroidsClient/www/ts/Level1.ts
@@ -6,6 +6,8 @@ module Astroids {
         player: Phaser.Sprite;
 
         otherPlayer: Phaser.Sprite;
+        leftKey: Phaser.Key;
+        rightKey: Phaser.Key;
 
         preload() {
             this.load.image('bg', 'assets/bg.png');
@@ -22,21 +24,37 @@ module Astroids {
             var randX = this.game.rnd.realInRange(0, this.world.width * 0.95);
             var randY = this.game.rnd.realInRange(0, this.world.height * 0.95);
             this.player = this.add.sprite(randX, randY, 'player');
+            this.player.anchor.set(0.5, 0.5);
+
+
+            this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+            this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
         }
 
         update() {
-            astroids.p2p.sendText(this.player.position.x + ';' + this.player.position.y + ';');
+
+            if (this.leftKey.isDown) {
+                this.player.angle -= 3;
+            }
+            else if (this.rightKey.isDown) {
+                this.player.angle += 3;
+            }
+            astroids.p2p.sendText(this.player.position.x + ';' + this.player.position.y + ';' + this.player.angle + ';');
+
         }
 
         onTextReceived(text: string) {
             console.log('game received ' + text);
             var messageArray = text.split(';');
-            var messageX:number = +messageArray[0];
-            var messageY:number = +messageArray[1];
-            
-            if (! this.otherPlayer) {
+            var messageX: number = +messageArray[0];
+            var messageY: number = +messageArray[1];
+            var messageAngle: number = +messageArray[2];
+
+            if (!this.otherPlayer) {
                 this.otherPlayer = this.add.sprite(messageX, messageY, 'player');
+                this.otherPlayer.anchor.set(0.5, 0.5);
             }
+            this.otherPlayer.angle = messageAngle;
         }
 
         onClick() {
