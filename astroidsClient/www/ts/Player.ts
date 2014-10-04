3c6469862e12427fe6cf2d402fad5fc8e5dfc072
Mirco Kraenz <prosingularity@yahoo.de>
2014-10-04 16:01:55 +0200

    ADDED: Bullets

--- astroidsClient/www/ts/Player.ts
+++ astroidsClient/www/ts/Player.ts
@@ -5,6 +5,7 @@ module Astroids {
 
         static preload(game: Phaser.Game) {
             game.load.image('player', 'assets/player040.png');
+            game.load.image('bullet', 'assets/player020.png');
         }
 
         private static UPDATE_ME_KEY: string = 'playerUpdateMe';
@@ -13,9 +14,26 @@ module Astroids {
         private static MAX_SPEED: number = 300;
         private static ACCELERATION: number = 300;
 
+        private static BULLET_SPEED: number = 400;
+        private static BULLET_COOLDOWN: number = 50;
+
+        private bullets: Phaser.Group;
+        private bulletReactivationTime: number = 0;
+
         constructor(game: Phaser.Game, x: number, y: number, private isLocal: boolean) {
             super(game, x, y, 'player');
             this.anchor.setTo(0.5, 0.5);
+
+            //  Our ships bullets
+            this.bullets = game.add.group();
+            this.bullets.enableBody = true;
+            this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+            //  All 40 of them
+            this.bullets.createMultiple(40, 'bullet');
+            this.bullets.setAll('anchor.x', 0.5);
+            this.bullets.setAll('anchor.y', 0.5);
+
             game.add.existing(this);
 
             game.physics.enable(this, Phaser.Physics.ARCADE);
@@ -25,6 +43,8 @@ module Astroids {
             if (!this.isLocal) {
                 astroids.p2p.receiveText(Player.UPDATE_ME_KEY, this.onUpdateMe, this);
             }
+
+            // game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
         }
 
         update() {
@@ -44,8 +64,12 @@ module Astroids {
                     this.game.physics.arcade.accelerationFromRotation(this.rotation,
                         Player.ACCELERATION, this.body.acceleration);
                 }
+
+                if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                    this.fireBullet();
+                }
                 astroids.p2p.sendText(Player.UPDATE_ME_KEY, this.angle + ';' + this.x + ';' + this.y + ';');
-                
+
                 this.screenWrap();
             }
         }
@@ -76,6 +100,23 @@ module Astroids {
             else if (this.y > this.game.height) {
                 this.y = 0;
             }
+        }
+
+        fireBullet() {
+
+            if (this.game.time.now > this.bulletReactivationTime) {
+                var bullet: Phaser.Sprite = this.bullets.getFirstExists(false);
+
+                if (bullet) {
+                    bullet.reset(this.body.x + this.width / 2, this.body.y + this.height / 2);
+                    bullet.lifespan = 2000;
+                    bullet.rotation = this.rotation;
+                    this.game.physics.arcade.velocityFromRotation(this.rotation,
+                        Player.BULLET_SPEED, bullet.body.velocity);
+                    this.bulletReactivationTime = this.game.time.now + Player.BULLET_COOLDOWN;
+                }
+            }
+
         }
     }
 }
